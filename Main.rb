00e0950a2b68680d1117dbb5e249ffddafd6c5a0
Jamie <lijamie19828@gmail.com>
2022-01-03 23:58:16 +0000

    refactor Main

--- Main.rb
+++ Main.rb
@@ -6,34 +6,34 @@ class Game
   def initialize
     @player1 = Player.new
     @player2 = Player.new
-    @Turns = Turn.new
-    @Questions = Questions.new
-    question1 = @Questions.new_question(1)
+    @turns = Turn.new
+    @questions = Questions.new
+    question1 = @questions.new_question(1)
     first_question(question1)
   end
 
   def first_question(question)
     @player1.lose_life unless question
-    @Turns.next_turn
+    @turns.next_turn
     puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
-    new_question = @Questions.new_question(@Turns.turn)
+    new_question = @questions.new_question(@turns.turn)
     handle_questions(new_question)
   end
 
   def handle_questions(question)
     unless question
-      @Turns.turn == 1 ? @player1.lose_life : @player2.lose_life
-      @Turns.next_turn
+      @turns.turn == 1 ? @player1.lose_life : @player2.lose_life
+      @turns.next_turn
     end
 
-    if @player1.lives > 0 && @player2.lives > 0
+    if @player1.lives.positive? && @player2.lives.positive?
       puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
-      new_question = @Questions.new_question(@Turns.turn)
+      new_question = @questions.new_question(@turns.turn)
       handle_questions(new_question)
     else
-      if @player1.lives == 0
+      if @player1.lives.zero?
         puts "Player 2 wins with a score of #{@player2.lives}"
-      elsif @player2.lives == 0
+      elsif @player2.lives.zero?
         puts "Player 1 wins with a score of #{@player1.lives}"
       end
       puts '----- GAME OVER -----'
